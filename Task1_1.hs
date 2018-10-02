884782898b5558eb15095227fd9312111a6d4b62
JBezrukova <bezrukovajulia961@gmail.com>
2018-10-02 17:19:24 +0300

    first commit

---
+++ Task1_1.hs
@@ -0,0 +1,42 @@
+module Task1_1 where
+
+import Todo(todo)
+
+data Term = IntConstant{ intValue :: Int }           -- числовая константа
+            | Variable{ varName :: String }          -- переменная
+            | Add{ lhv :: Term, rhv :: Term } -- бинарная операция
+            | Sub{ lhv :: Term, rhv :: Term } -- бинарная операция
+            | Mult{ lhv :: Term, rhv :: Term } -- бинарная операция
+            deriving(Show,Eq)
+
+-- Для бинарных операций необходима не только реализация, но и адекватные
+-- ассоциативность и приоритет
+(|+|) :: Term -> Term -> Term
+(|+|) (IntConstant l) (IntConstant r) = IntConstant (l + r)
+(|+|) l r = Add l r
+(|-|) :: Term -> Term -> Term
+(|-|) (IntConstant l) (IntConstant r) = IntConstant (l - r)
+(|-|) l r = Sub l r
+(|*|) :: Term -> Term -> Term
+(|*|) (IntConstant l) (IntConstant r) = IntConstant (l * r)
+(|*|) l r = Mult l r
+infixl 6 |+|, |-|
+infixl 7 |*|
+-- Заменить переменную `var` на `replacement`
+-- во всём выражении `expression`
+replaceVar :: String -> Term -> Term -> Term
+replaceVar var replacement Variable {varName = varName'} | varName' == var = replacement
+                                                         | otherwise = Variable varName'
+replaceVar var replacement Add{lhv = l, rhv = r}  = Add  (replaceVar var replacement l) (replaceVar var replacement r)
+replaceVar var replacement Sub{lhv = l, rhv = r}  = Sub  (replaceVar var replacement l) (replaceVar var replacement r)
+replaceVar var replacement Mult{lhv = l, rhv = r} = Mult (replaceVar var replacement l) (replaceVar var replacement r)
+
+
+-- Посчитать значение выражения `Term`
+-- если оно состоит только из констант (и упрощает его по мере возможности, если нет).
+evaluate :: Term -> Term
+evaluate IntConstant {intValue = a}                                                 = IntConstant a
+evaluate Variable {varName = a}                                                     = Variable a
+evaluate Add  {lhv = l,                          rhv = r}                           =  ( (|+|) (evaluate l) (evaluate r))
+evaluate Sub  {lhv = l,                          rhv = r}                           =  ( (|-|) (evaluate l) (evaluate r))
+evaluate Mult {lhv = l,                          rhv = r}                           =  ( (|*|) (evaluate l) (evaluate r))
---
+++ Task1_2.hs
@@ -0,0 +1,66 @@
+module Task1_2 where
+
+import Todo(todo)
+
+-- синус числа (формула Тейлора)
+sin' :: Double -> Double
+sin' x 
+
+
+
+-- косинус числа (формула Тейлора)
+cos' :: Double -> Double
+cos' x 
+
+
+-- наибольший общий делитель двух чисел
+gcd' :: Integer -> Integer -> Integer
+gcd' x y | x == 0 = abs y
+         | y == 0 = abs x
+         | (abs x < abs y) = gcd' y x
+         | otherwise = gcd' y (x `mod` y)
+
+-- существует ли полный целочисленный квадрат в диапазоне [from, to)?
+doesSquareBetweenExist :: Integer -> Integer -> Bool
+doesSquareBetweenExist from to = (ceiling $ sqrt $ fromIntegral from) < (floor $ sqrt $ fromIntegral to)
+
+-- является ли дата корректной с учётом количества дней в месяце и
+-- вискокосных годов?
+isDateCorrect :: Integer -> Integer -> Integer -> Bool
+isDateCorrect day month year = todo
+
+-- возведение числа в степень, duh
+-- готовые функции и плавающую арифметику использовать нельзя
+pow :: Integer -> Integer -> Integer
+pow x y | y == 0 = 1
+		| even y = result
+		| odd y  = x * result
+      	  where result = pow (x * x) (y `div` 2)
+
+-- является ли данное число простым?
+isPrime :: Integer -> Bool
+isPrime x | x <= 1    = False
+		  | x == 2    = True
+          | otherwise = check x 2
+
+check :: Integer -> Integer -> Bool
+check a i | i == round(sqrt $ fromIntegral a) && a `mod` i == 0 = False
+          | i == round(sqrt $ fromIntegral a) && a `mod` i /= 0 = True
+		  | a `mod` i == 0                                      = False
+		  | otherwise                                           = check a (i + 1)
+
+type Point2D = (Double, Double)
+
+-- рассчитайте площадь многоугольника по формуле Гаусса
+-- многоугольник задан списком координат
+shapeArea :: [Point2D] -> Double
+shapeArea points = todo
+
+-- треугольник задан своими координатами.
+-- функция должна вернуть 
+--  0, если он тупоугольный
+--  1, если он остроугольный
+--  2, если он прямоугольный
+--  -1, если это не треугольник
+triangleKind :: Point2D -> Point2D -> Point2D -> Integer
+triangleKind a b c = todo
