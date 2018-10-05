664c365697f6164a676c5905ce13fb36000374a2
JBezrukova <bezrukovajulia961@gmail.com>
2018-10-05 21:30:27 +0300

    isDateCorrect changed

--- Task1_2.hs
+++ Task1_2.hs
@@ -22,32 +22,47 @@ doesSquareBetweenExist from to = (ceiling $ sqrt $ fromIntegral from) <= (floor 
 -- является ли дата корректной с учётом количества дней в месяце и
 -- вискокосных годов?
 isDateCorrect :: Integer -> Integer -> Integer -> Bool
-isDateCorrect day month year | month `mod` 2 /= 0 || month == 8 && day >= 1 && day <= 31 = True
+{-isDateCorrect day month year | month `mod` 2 /= 0 || month == 8 && day >= 1 && day <= 31 = True
                              | month `mod` 2 == 0 && month /= 2 && day >= 1 && day <= 30 = True
-                             | year `mod` 4 == 0 && month == 2 && day >= 1 && day <= 29 = True
-                             | year `mod` 4 /= 0 && month == 2 && day >= 1 && day <= 28 = True
+                             | yearIsLeap year == True  && month == 2 && day >= 1 && day <= 29 = True
+                             | yearIsLeap year == False && month == 2 && day >= 1 && day <= 28 = True
+                             | otherwise = False
+-}
+isDateCorrect day month year | month < 1 || month > 12 = False
+                             | year  < 0 = False
+                             | month == 2 = leapYearCheck day year
+                             | monthIsFor31Day month == True  && day >= 1 && day <= 31 = True
+                             | monthIsFor31Day month == False && day >= 1 && day <= 30 = True
                              | otherwise = False
 
+monthIsFor31Day month | month < 8 && month `mod` 2 /= 0 = True
+                      | month >= 8 && month `mod` 2 == 0 = True
+                      | otherwise = False
 
+leapYearCheck day year | year `mod` 400 == 0 && day >= 1 && day <= 29 = True
+                       | year `mod` 100 == 0 && (day < 1 || day > 28) = False
+                       | year `mod` 100 == 0 && day >= 1 && day <= 28 = True
+                       | year `mod` 4   == 0 && day >= 1 && day <= 29 = True
+                       | otherwise = False
 -- возведение числа в степень, duh
 -- готовые функции и плавающую арифметику использовать нельзя
 pow :: Integer -> Integer -> Integer
 pow x y | y == 0 = 1
-		    | even y = result
-		    | odd y  = x * result
-      	  where result = pow (x * x) (y `div` 2)
+        | even y = result
+        | odd y  = x * result
+          where result = pow (x * x) (y `div` 2)
 
 -- является ли данное число простым?
 isPrime :: Integer -> Bool
 isPrime x | x <= 1    = False
-		      | x == 2    = True
+          | x == 2    = True
           | otherwise = check x 2
 
 check :: Integer -> Integer -> Bool
 check a i | a `mod` i == 0                                      = False
           | i == round(sqrt $ fromIntegral a) && a `mod` i /= 0 = True
-		      | i == round(sqrt $ fromIntegral a) && a `mod` i == 0 = False
-		      | otherwise                                           = check a (i + 1)
+          | i == round(sqrt $ fromIntegral a) && a `mod` i == 0 = False
+          | otherwise                                           = check a (i + 1)
 
 type Point2D = (Double, Double)
 
